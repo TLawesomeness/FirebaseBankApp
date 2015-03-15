98bd3ad4015ca9613eb30220b49172b7939717ec
Tommyluo <tluo12321@yahoo.com>
2015-03-14 21:54:00 -0700

    bug fixes, working now

--- client/main.js
+++ client/main.js
@@ -4,77 +4,58 @@ $(document).ready(function() {
   var balDataRef = new Firebase('https://bankapp.firebaseio.com/balances');
   var depoDataRef = new Firebase('https://bankapp.firebaseio.com/deposits');
   var withDataRef = new Firebase('https://bankapp.firebaseio.com/withdraws');
-  var feesDataRef = new Firebase('https://bankapp.firebaseio.com/fees');
+  var debtDataRef = new Firebase('https://bankapp.firebaseio.com/fees');
 
   var balance = 0;
 
-  $('#depo').click(function() {
+  $('#depobtn').click(function() {
     var amount = $('#input').val();
     var date = new Date().toDateString();
-    console.log(amount);
-    balance += parseInt(amount);
 
     depoDataRef.push({amount: amount, date: date});
+    balance += parseInt(amount);
     balDataRef.update({balance: balance});
   });
 
-  depoDataRef.on('child_added', function(data) {
-    var depoData = data.val();
-    showDeposit(depoData.amount, depoData.date);
-
-    function showDeposit(amount, date) {
-      var $depositLi = $('<li>');
-      $depositLi.addClass('depositli');
-      $depositLi.text('$' + amount + ' ' + date);
-      $('#dep').append($depositLi);
-    }
-  });
-
-  $('#with').click(function() {
+  $('#withbtn').click(function() {
     var amount = $('#input').val();
     var date = new Date().toDateString();
-    console.log(amount);
-    balance -= parseInt(amount);
 
     withDataRef.push({amount: amount, date: date});
-
+    balance -= parseInt(amount);
     var debt = 0;
     if (balance < 0) {
       debt -= 50;
       balance += debt;
-      feesDataRef.push({debt:debt, date: date});
+      debtDataRef.push({debt:debt, date: date});
     }
     balDataRef.update({balance: balance});
   });
 
   balDataRef.on('value', function(data) {
     var bal = data.val();
-    console.log('current balance: ' + balance);
     $('#balance').text('Balance: $' + bal.balance);
+    balance = bal.balance;
+  });
+
+  depoDataRef.on('child_added', function(data) {
+    var depoData = data.val();
+    displayData(depoData.amount, depoData.date, 'dep');
   });
 
   withDataRef.on('child_added', function(data) {
     var withData = data.val();
-    showWithdraw(withData.amount, withData.date);
-
-    function showWithdraw(amount, date) {
-      var $withdrawLi = $('<li>');
-      $withdrawLi.addClass('withdrawli');
-      $withdrawLi.text('$' + amount + ' ' + date);
-      $('#wit').append($withdrawLi);
-    }
+    displayData(withData.amount, withData.date, 'wit');
   });
 
-  feesDataRef.on('child_added', function(data) {
+  debtDataRef.on('child_added', function(data) {
     var feesData = data.val();
-    showDebt(feesData.debt, feesData.date);
-
-    function showDebt(debt, date) {
-      var $debtLi = $('<li>');
-      $debtLi.addClass('debtli');
-      $debtLi.text('$' + debt + ' ' + date);
-      $('#fees').append($debtLi);
-    }
+    displayData(feesData.debt, feesData.date, 'fees');
   });
 
+  function displayData(amount, date, id) {
+    var $li = $('<li>');
+    $li.text('$' + amount + ' ' + date);
+    $('#' + id).append($li);
+  }
 });
